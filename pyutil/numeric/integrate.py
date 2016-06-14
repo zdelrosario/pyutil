4bf6cf37c7fe9afdb81e068f0cc7cfc0de7803b5
zach <zdelrosario@outlook.com>
2016-06-14 10:35:37 -0700

    added cubature_data to return cubature nodes and weights

--- pyutil/numeric/integrate.py
+++ pyutil/numeric/integrate.py
@@ -8,8 +8,21 @@ import numpy as np
 import copy
 from numpy.polynomial.hermite import hermgauss
 from numpy.polynomial.legendre import leggauss
+from operator import mul
 
 from util import multi_index
+
+# Convenience function
+def prod(L):
+    """Returns the product of an iterable's elements
+    Usage
+        r = prod(L)
+    Arguments
+        L = an iterable of elements with binary multiplication
+    Returns
+        r = result of product
+    """
+    return reduce(mul, L, 1)
 
 # Partial product of weights
 def part_prod(fcn,Xi,x,w):
@@ -20,7 +33,34 @@ def part_prod(fcn,Xi,x,w):
         X_c.append(x[Xi[i]])
     return scale * fcn(X_c)
 
-# Generic m-D cubature function
+# Currently takes fixed number of points per dimension,
+# could extend to vary points per dimension easily
+def cubature_data(n,m,rule):
+    """
+    Usage
+        X, W = cubature_data(n,m,rule)
+    Arguments
+        n    = points per dimension
+        m    = dimensions
+        rule = quadrature nodes of form {x, w = rule(n)}
+    Returns
+        X    = quadrature nodes
+        W    = quadrature weights
+    """
+    # Load 1-D nodes and weights
+    x, w = rule(n)
+    # Generate multi-index list
+    E = multi_index([n]*m)
+    # Generate m-D nodes and weights
+    X = []; W = []
+    for e in E:
+        X.append([x[i] for i in e])
+        W.append(prod([w[i] for i in e]))
+    # Return
+    return X, W
+
+# Currently takes fixed number of points per dimension,
+# could extend to vary points per dimension easily
 def cubature(fcn, n, m, rule):
     """Perform cubature based on a provided 
     quadrature rule
@@ -35,7 +75,7 @@ def cubature(fcn, n, m, rule):
     Returns
         res = result of cubature
     """
-    # Load 1-D points/weights
+    # Load 1-D nodes and weights
     x, w = rule(n)
     # Generate multi-index list
     E = multi_index([n]*m)
@@ -45,13 +85,13 @@ def cubature(fcn, n, m, rule):
         res += part_prod(fcn,E[i],x,w)
     return res
 
-def integrate_hermite(fcn, n, m):
+def cubature_hermite(fcn, n, m):
     return cubature(fcn, n, m, hermgauss)
 
-def integrate_legendre(fcn, n, m):
+def cubature_legendre(fcn, n, m):
     return cubature(fcn, n, m, leggauss)
 
-def integrate_rule(fcn, n, m, flag):
+def cubature_rule(fcn, n, m, flag):
     """Integrates an m-dimensional function on [-1,1]
     via a tensor-product cubature rule
     Usage
@@ -65,9 +105,9 @@ def integrate_rule(fcn, n, m, flag):
         res = result of cubature
     """
     if flag.upper() == 'HERMITE':
-        return integrate_hermite(fcn, n, m)
+        return cubature_hermite(fcn, n, m)
     elif flag.upper() == 'LEGENDRE':
-        return integrate_legendre(fcn, n, m)
+        return cubature_legendre(fcn, n, m)
     else:
         raise ValueError("Quadrature rule '"+flag+"' unrecognized")
 
