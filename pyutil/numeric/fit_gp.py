7cd1509c43c02de3e12d7ea8fcee054f1eb4c43c
zach <zdelrosario@outlook.com>
2016-06-30 14:00:02 -0700

    fixed import error

--- pyutil/numeric/fit_gp.py
+++ pyutil/numeric/fit_gp.py
@@ -6,7 +6,7 @@
 ##################################################
 
 import numpy as np
-from pyutil.numeric import col, quad, vec, unvec
+from util import col, quad, vec, unvec
 from numpy.random import rand, randn
 from scipy.linalg import inv
 from numpy import sqrt
