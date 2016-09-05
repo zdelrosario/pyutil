5b41f1d519c9d6eb44a514c0db3957513376b70f
zach <zdelrosario@outlook.com>
2016-09-05 10:23:33 -0700

    added boilerplate code folder; command line arguments

---
+++ pyutil/boilerplate/cmd_arg.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+import sys
+
+if len(sys.argv) == 1:
+    print("Usage:")
+    print("    (Usage string goes here...)")
+else:
+    # Handle command line arguments
+    # sys.argv[0] == script name
+    # sys.argv[1] == argument #1
+    # sys.argv[2] == argument #2
+    # etc.
+    pass
