e431a5ba5b5b51c667ab1629e21f4a7f0e02702f
Zach <zdelrosario@outlook.com>
2017-11-13 11:11:53 -0800

    Hotelling's T^2 test

--- pyutil/numeric/__init__.py
+++ pyutil/numeric/__init__.py
@@ -6,7 +6,7 @@ from integrate import normalize_function
 from interior import constrained_opt
 from poly import tpolyval, tlegendre, tfcn, tleg
 from util import *
-from stats import dr_sir, dr_save
+from stats import dr_sir, dr_save, hotelling
 from dr_tools import dr_smoothness, inter, dr_anova, dr_sobol, array_comp
 from noise import ecnoise
 from print_progress import print_progress
